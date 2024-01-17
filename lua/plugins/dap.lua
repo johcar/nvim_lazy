901d083069f1c5f584ba06eabe9747b50413f336
johcar <johancarlstrom@gmail.com>
2024-01-17 23:24:15 +0100

    Add override to change behavior of dap/dapui when running tests
    - Don't listen to terminated/close events which results in dapui closing right after finishing the test
    - Instead just open the dap repl(instead of the whole dapui) and show me the test results without closing it directly

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,40 @@
+return {
+  "mfussenegger/nvim-dap",
+
+  dependencies = {
+
+    -- Override dap-ui defaults - in practice we disable the close events
+    {
+      "rcarriga/nvim-dap-ui",
+      -- stylua: ignore
+      keys = {
+        { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
+        { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
+      },
+      opts = {},
+      config = function(_, opts)
+        -- setup dap config by VsCode launch.json file
+        -- require("dap.ext.vscode").load_launchjs()
+        local dap = require("dap")
+        local dapui = require("dapui")
+        dapui.setup(opts)
+        --[[  
+        dap.listeners.after.event_initialized["dapui_config"] = function()
+          dapui.open({})
+        end
+        dap.listeners.before.event_terminated["dapui_config"] = function()
+          dapui.close({})
+        end
+        dap.listeners.before.event_exited["dapui_config"] = function()
+          dapui.close({})
+        end]]
+      end,
+    },
+  },
+  config = function(_, _)
+    local dap = require("dap")
+    dap.listeners.after.event_initialized["dap_config"] = function()
+      dap.repl.open({})
+    end
+  end,
+}
