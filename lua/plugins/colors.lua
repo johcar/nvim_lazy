ec2b24d7d1f7a6495caae13506603c4fc7e16c76
johcar <johancarlstrom@gmail.com>
2024-01-17 23:42:25 +0100

    Change colorscheme

---
+++ lua/plugins/colors.lua
@@ -0,0 +1,21 @@
+return {
+  -- Add onedark
+  {
+    "navarasu/onedark.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("onedark").setup({
+        style = "darker",
+      })
+    end,
+  },
+
+  -- Configure LazyVim to load onedark
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
